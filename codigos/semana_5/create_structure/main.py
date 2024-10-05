753057dc516355bb5c7ae16b0493336ef9a0b15d
Josef Leyva <jalm_x@hotmail.com>
2024-10-05 17:19:25 -0600

    add: sorty v2

--- codigos/semana_5/create_structure/main.py
+++ codigos/semana_5/create_structure/main.py
@@ -10,5 +10,69 @@
 # ---> util.py
 
 ## Funcionamiento
-# strc_py duplicate
-# strc_py
+# strc duplicate
+# strc
+
+from os import path, makedirs
+import sys
+
+
+def create_file(name_file: str, path_file: str = "") -> str:
+    """Create a file with a name
+    Args:
+        name_file (str): name file to create
+        path_file (str): path of file to create
+
+    Returns:
+        str: full path from file
+    """
+    full_path = path.join(path_file, name_file)
+    open(full_path, "w+")
+
+    return path.abspath(full_path)
+
+
+def create_dir(name_dir: str, path_dir: str = ""):
+    """Create a folder
+
+    Args:
+        name_dir (str): _description_
+        path_dir (str, optional): _description_. Defaults to "".
+    """
+    full_path = path.join(path_dir, name_dir)
+    makedirs(full_path, exist_ok=True)
+    return path.abspath(full_path)
+
+
+def cli():
+    values = sys.argv  ## Lee los argumentos pasados por terminal
+    values = values[1:]  # estoy quitando el primer valor y me quedo con todo lo demás
+    if len(values) > 0:
+        return values[0]
+    return ""
+
+
+if __name__ == "__main__":
+    files = ["__init__.py", "__main__.py", "util.py"]
+
+    folders = ["lib"]
+
+    name_project = cli()  # guardo el nombre del proyecto
+    path_base =""
+    if name_project:
+        files.insert(0, name_project)  # inserto al inicio del array el nombre del proyecto
+        path_base = create_dir(name_dir=name_project)
+
+    print(files)
+
+    for index, element in enumerate(files):
+        if index != len(files) -1:
+            if not element.endswith(".py"):
+                element += ".py"
+            create_file(element, path_base)
+        else:
+            dir_lib = create_dir(folders[0],path_dir=path_base)
+            create_file(element, dir_lib)
+
+    print(f"Project created at: {path_base or "here"}")
+
---
+++ codigos/semana_5/sorty/sorty.py
@@ -0,0 +1,65 @@
+from os import path, makedirs, listdir
+from pathlib import Path
+import shutil
+import sys  # aqui viene el método para mover archivos
+
+
+files_ext = {
+    "pdf": {"ext": [".pdf"]},
+    "word": {"ext": [".doc", ".docx"]},
+    "imgs": {"ext": [".png", ".jpeg", ".jpg", "webm", ".gif", ".svg"]},
+    "others": {"ext": []},
+}
+
+
+def create_dir(name_dir: str, path_dir: str = ""):
+    """Create a folder
+
+    Args:
+        name_dir (str): _description_
+        path_dir (str, optional): _description_. Defaults to "".
+    """
+    full_path = path.join(path_dir, name_dir)
+    makedirs(full_path, exist_ok=True)
+    return path.abspath(full_path)
+
+
+def cli():
+    values = sys.argv  ## Lee los argumentos pasados por terminal
+    values = values[1:]  # estoy quitando el primer valor y me quedo con todo lo demás
+    if len(values) > 0:
+        return values[0]
+    return ""
+
+
+if __name__ == "__main__":
+
+    path_dir_base = "" # la ruta la carpeta que vamos a ordenar
+
+    # creación de las carpetas
+    for folder in files_ext:
+        files_ext[folder]["path"] = create_dir(folder)
+
+    # leo todos los archivos de la carpetas
+    files = listdir(path.abspath(path_dir_base))
+    count = 0
+
+    for f in files:
+
+        if not path.isdir(path.join(path_dir_base, f)):
+
+            name_folder = "others"
+
+            for folder in files_ext:
+
+                if Path(f).suffix in files_ext.get(folder)["ext"]:
+                    name_folder = folder
+                    break
+
+            shutil.move(
+                path.join(path_dir_base, f),  # ruta completa con nombre
+                path.join(path_dir_base, name_folder, f),  # ruta nueva con el nombre
+            )
+            count += 1
+
+    print(f"{count} moved")
